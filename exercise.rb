ea32e25dfcbb1545610965b41c86fdb0020cdbef
Michael Wong <michael@uglybunnies.com>
2023-02-22 13:37:42 -0800

    completed methods with test passed

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,15 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+    # replace words with 5 or more letters in the 'str' argument
+      str.gsub(/\b[A-Za-z]{5,}\b/) do |word|
+      # Check of the word is capitalized and replace with "Marklar" otherwise replace with "marklar"
+        if word.capitalize == word
+          "Marklar"
+        else
+          "marklar"
+        end
+    end
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -12,7 +20,29 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    # initialize variables for the first two terms in the sequence
+    term1 = 1
+    term2 = 1
+
+    # initialize a variable to keep track of the sum of even numbers
+    even_sum = 0
+
+    # iterate from the third term up to the nth term
+    for i in 3..nth
+      # calculate the next term in the sequence as the sum of the previous two terms
+      next_term = term1 + term2
+      
+      # check if the next term is even, and add it to the sum if it is
+      if next_term % 2 == 0
+        even_sum += next_term
+      end
+      
+      # update the values of the previous two terms
+      term1, term2 = term2, next_term
+    end
+
+    # return the sum of even numbers in the sequence
+    return even_sum
   end
 
 end
--- exercise_spec.rb
+++ exercise_spec.rb
@@ -5,25 +5,25 @@ require './exercise'
 describe Exercise do
   describe "#marklar" do
     it "should return a marklar'd string" do
-      Exercise.marklar("The quick brown fox").must_equal "The marklar marklar fox"
+      _(Exercise.marklar("The quick brown fox")).must_equal "The marklar marklar fox"
     end
 
     it "should capitalize Marklar when replacing capitalized words" do
-      Exercise.marklar("Down goes Frazier").must_equal "Down goes Marklar"
-      Exercise.marklar("Monday is Martin Luther King day").must_equal "Marklar is Marklar Marklar King day"
+      _(Exercise.marklar("Down goes Frazier")).must_equal "Down goes Marklar"
+      _(Exercise.marklar("Monday is Martin Luther King day")).must_equal "Marklar is Marklar Marklar King day"
     end
 
     it "should keep end-of-word punctuation intact" do
-      Exercise.marklar("How is the weather today? I have not been outside.").must_equal "How is the marklar marklar? I have not been marklar."
+      _(Exercise.marklar("How is the weather today? I have not been outside.")).must_equal "How is the marklar marklar? I have not been marklar."
     end
   end
 
   describe "#even_fibonacci" do
     it "should sum the even numbers in a Fibonacci sequence" do
-      Exercise.even_fibonacci(5).must_equal 2
-      Exercise.even_fibonacci(11).must_equal 44
-      Exercise.even_fibonacci(19).must_equal 3382
-      Exercise.even_fibonacci(35).must_equal 4_613_732
+      _(Exercise.even_fibonacci(5)).must_equal 2
+      _(Exercise.even_fibonacci(11)).must_equal 44
+      _(Exercise.even_fibonacci(19)).must_equal 3382
+      _(Exercise.even_fibonacci(35)).must_equal 4_613_732
     end
   end
 end
